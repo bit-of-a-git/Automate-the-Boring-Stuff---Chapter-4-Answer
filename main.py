4a36b2e1befd375fe833c5893868f3f1c1f6ca1f
bit-of-a-git <92552641+bit-of-a-git@users.noreply.github.com>
2022-08-16 17:06:41 +0100

    ATBS Chapter 4 Answer
    My answer for the question at the end of Chapter 4, Automate the Boring Stuff

---
+++ main.py
@@ -0,0 +1,18 @@
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+rows = len(grid)
+columns = len(grid[0])
+
+for x in range(columns):
+    for y in range(rows):
+        print(grid[y][x], end='')
+    # prints a new line after every row
+    print("")
